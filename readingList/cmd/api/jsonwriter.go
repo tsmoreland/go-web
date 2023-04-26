8b514cc2b946465ca735be0af3d3c144642717b5
Terry Moreland <tsmoreland@live.com>
2023-04-26 23:22:20 +0100

    "handle" error from json writer by ignoring it, should change that at a later date to maybe return the error instead

--- readingList/cmd/api/jsonwriter.go
+++ readingList/cmd/api/jsonwriter.go
@@ -11,7 +11,7 @@ func (svc *service) writeJSON(w http.ResponseWriter, status int, data any) error
 	} else {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(status)
-		w.Write(js)
+		_, _ = w.Write(js)
 		return nil
 	}
 }
