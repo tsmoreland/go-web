f31d75a4fd590f50da89125ab005d4ce52241ffb
Terry Moreland <tsmoreland@live.com>
2023-04-26 23:21:08 +0100

    address warnings in json tags

--- readingList/internal/data/books.go
+++ readingList/internal/data/books.go
@@ -8,7 +8,7 @@ type Book struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Published int       `json:"published,omitempty""`
+	Published int       `json:"published,omitempty"`
 	Pages     int       `json:"pages,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Rating    float32   `json:"rating,omitempty"`
