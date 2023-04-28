1faff9bf4fff5068c30e7b65e59b555a0d3937f1
Terry Moreland <tsmoreland@live.com>
2023-04-29 00:15:23 +0100

    updates to repository interface

--- readingList/internal/data/repository.go
+++ readingList/internal/data/repository.go
@@ -2,5 +2,7 @@ package data
 
 type Repository interface {
 	Migrate() error
+	Ping() error
+	InsertOne(title string, published int, pages int, rating float64, genres []string) (*Book, error)
 	Close() error
 }
