b88a789b6383b2d920b4445b2ef70efda090afef
Terry Moreland <tsmoreland@live.com>
2023-04-30 20:36:18 +0100

    initial reading list api client, to be used with web app

---
+++ readingList/internal/models/readinglistclient.go
@@ -0,0 +1,71 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Book struct {
+	ID        int64    `json:"id"`
+	Title     string   `json:"title"`
+	Published int      `json:"published"`
+	Pages     int      `json:pages`
+	Genres    []string `json:"geres"`
+	Rating    float64  `json:"rating"`
+}
+
+type BookResponse struct {
+	Book *Book `json:"book"`
+}
+
+type BooksResponse struct {
+	Books *[]Book `json:"books"`
+}
+
+type ReadingListClient struct {
+	Endpoint string
+}
+
+func (c *ReadingListClient) GetAll() (*[]Book, error) {
+	resp, err := http.Get(c.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %d", resp.StatusCode)
+	}
+	var booksResponse BooksResponse
+	if err := ReadJSONObject(resp.Body, &booksResponse); err != nil {
+		return nil, err
+	}
+	return booksResponse.Books, nil
+}
+
+func (c *ReadingListClient) Get(id int64) (*Book, error) {
+	url := fmt.Sprintf("%s/%d", c.Endpoint, id)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %d", resp.StatusCode)
+	}
+
+	var bookResponse BookResponse
+	if err := ReadJSONObject(resp.Body, &bookResponse); err != nil {
+		return nil, err
+	}
+	return bookResponse.Book, nil
+}
