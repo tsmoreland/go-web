607fd17846a9b54344fc300f812dbc2013d8ac21
Terry Moreland <tsmoreland@live.com>
2023-05-02 00:09:24 +0100

    home method handler using fmt.Fprintf - to be changed later

--- readingList/cmd/web/handler.go
+++ readingList/cmd/web/handler.go
@@ -1,8 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	books, err := app.client.GetAll()
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	_, _ = fmt.Fprintf(w, "<html><head><title>Reading List</title></head><body><h1>Reading List</h1><ul>")
+
+	for _, book := range *books {
+		_, _ = fmt.Fprintf(w, "<li>%s (%d)</li>", book.Title, book.Pages)
+	}
+
+	_, _ = fmt.Fprintf(w, "</ul></body></html>")
 	w.WriteHeader(http.StatusOK)
 }
 
