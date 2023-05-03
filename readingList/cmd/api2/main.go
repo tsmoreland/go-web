6590b56052fa2bc4d70051c80086517b6fe5ed71
Terry Moreland <tsmoreland@live.com>
2023-05-03 21:39:05 +0100

    add generated v2 API based on code generated via openapi (smartbear swagger tools)

--- readingList/cmd/api2/main.go
+++ readingList/cmd/api2/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Pending...")
+	log.Printf("Server started")
+
+	api := NewApi()
+	router := NewRouter(api)
+	log.Fatal(http.ListenAndServe(":9000", router))
 }
