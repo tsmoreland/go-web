b7c393c8f9305012d568fd6c1ef6d5f6dc74de78
Terry Moreland <tsmoreland@live.com>
2023-05-05 00:16:44 +0100

    implement get all books

--- readingList/cmd/api2/api_books.go
+++ readingList/cmd/api2/api_books.go
@@ -39,8 +39,16 @@ func (api *Api) DeleteBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func (api *Api) GetAllBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	books, err := api.repository.FindAll(true)
+	if err != nil {
+		api.writeProblemDetails(w, r, "server error", http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err = api.writeJSON(w, http.StatusOK, NewBooksEnvelope(books)); err != nil {
+		api.writeProblemDetails(w, r, "server error", http.StatusInternalServerError, err.Error())
+		return
+	}
 }
 
 func (api *Api) GetBook(w http.ResponseWriter, r *http.Request) {
