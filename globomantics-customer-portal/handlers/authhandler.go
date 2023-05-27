225186cbebbc24033e5d237a2d42407e7c95375f
Terry Moreland <tsmoreland@live.com>
2023-05-27 01:35:33 +0100

    add stubs for login/logout handlers

---
+++ globomantics-customer-portal/handlers/authhandler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import "net/http"
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	_ = w
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	_ = w
+}
