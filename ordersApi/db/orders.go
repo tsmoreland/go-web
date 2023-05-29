f65f2297a42d63713e58c01b0a30ea74219f0f18
Terry Moreland <terry.s.moreland@gmail.com>
2023-05-29 14:27:53 +0100

    replace map with sync.Map to safer multi-threaded requests

--- ordersApi/db/orders.go
+++ ordersApi/db/orders.go
@@ -3,30 +3,37 @@ package db
 import (
 	"fmt"
 	"github.com/tsmoreland/go-web/ordersApi/models"
+	"sync"
 )
 
 type OrderDB struct {
-	placedOrders map[string]models.Order
+	placedOrders sync.Map
 }
 
 // NewOrders creates a new empty order service
 func NewOrders() *OrderDB {
-	return &OrderDB{
-		placedOrders: make(map[string]models.Order),
-	}
+	return &OrderDB{}
 }
 
 // FindById order for a given id, if one exists
 func (o *OrderDB) FindById(id string) (models.Order, error) {
-	order, ok := o.placedOrders[id]
+	order, ok := o.placedOrders.Load(id)
 	if !ok {
 		return models.Order{}, fmt.Errorf("no order found for %s order id", id)
 	}
 
-	return order, nil
+	return toOrder(order), nil
 }
 
 // Upsert creates or updates an order in the orders DB
 func (o *OrderDB) Upsert(order models.Order) {
-	o.placedOrders[order.ID] = order
+	o.placedOrders.Store(order.ID, order)
+}
+
+func toOrder(maybeOrder any) models.Order {
+	order, ok := maybeOrder.(models.Order)
+	if !ok {
+		panic(fmt.Errorf("error casting %v to order", maybeOrder))
+	}
+	return order
 }
